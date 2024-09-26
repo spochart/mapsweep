fe5739f3be19b02c7f5763bd9ffec904257cba48
Noah Spochart <noah@spochart.co>
2024-09-26 15:58:34 -0700

    Installed Sorcery on User

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module Mapsweep
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |generate|
+      generate.assets(false)
+      generate.helper(false)
+      generate.stylesheets(false)
+    end
   end
 end
